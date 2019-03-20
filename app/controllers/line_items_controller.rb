05a034f80220ba366a8bfe0f2b4288bc96199604
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2019-03-20 09:15:36 +0100

    Add columns to line table

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
-
+    
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
